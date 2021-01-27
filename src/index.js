c84eab7ba9805d01c6f1302c26468d70f605e5a6
i-zanis <ioanlafazanis@gmail.com>
2021-01-27 06:34:35 +0000

    adjusted Square

--- src/index.js
+++ src/index.js
@@ -3,36 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-        return (
-            <button
-            className = "square"
-            onClick={() => this.props.onClick()}
-            >
-        {this.props.value}
-        </button>
-        );
-    }
-}
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-        squares: Array(9).fill(null),
+      squares: Array(9).fill(null),
     };
-}  
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
     return (
       <Square
@@ -49,19 +45,13 @@ constructor(props) {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
         </div>
       </div>
     );
@@ -90,3 +80,4 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
