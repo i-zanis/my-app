5fad8e048b42a507a6c1e8d6205f96c3616779ec
i-zanis <ioanlafazanis@gmail.com>
2021-02-06 22:12:35 +0000

    changed Class Board

--- src/index.js
+++ src/index.js
@@ -28,28 +28,95 @@ class Board extends React.Component {
     });
     }
 
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-  onst winner = calculateWinner(this.state.squares);
-  let status;
-  if (winner) {
-        status = 'Winner: ' + winner;
-  } else {
-        status = 'Next player: ' (this.state.xIsnext ? 'X' :
-        'O');
-  }
-  }
-
+  renderSquare(i) 
+  handleClick(i) {
+        const Square = this.state.squares.slice();
+        if (calculateWinner(squares) || squares]i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: sqaures,
+            xInsNext: !this.state.xIsNext;
+        });
+        }
+        renderSquare(i) {
+            return (
+                <Square
+                value = {this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+                />
+            );
+        }
+        render() {
+            const winner = calculateWinner(this.state.squares);
+            let status;
+            if (winner) {
+                status = 'Winner: ' + winner;
+            }else
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); 
+        }
+        return (
+            <div> 
+            <dive className="status">{status}</div>
+            <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            </div>
+            <div className= "board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            </div>
+            <div className = "board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            </div>
+            </div>
+        );
+        }
+        }
+  
+  history = [
+  // Before first move
+  {
+    squares: [
+      null, null, null,
+      null, null, null,
+      null, null, null,
+    ]
+  },
+  // After first move
+  {
+    squares: [
+      null, null, null,
+      null, 'X', null,
+      null, null, null,
+    ]
+  },
+  // After second move
+  {
+    squares: [
+      null, null, null,
+      null, 'X', null,
+      null, null, 'O',
+    ]
+  },
+  // ...
+]
 class Game extends React.Component {
-  render() {
+constructor(props) {
+    super(props);
+    this.state = {
+        history: [{
+            squares: Array(9).fill(null),
+        }],
+        xIsnext:true,
+    };
+}  
+render() {
     return (
       <div className="game">
         <div className="game-board">
